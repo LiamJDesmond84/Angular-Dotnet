aa9cf4706ce0bbc9830dca78086b5d45de35149a
Liam James Desmond <liamjdesmond84@gmail.com>
2023-07-03 18:43:01 -0500

    Added CORS to backend API

--- SuperHeroAPI/SuperHeroAPI/Program.cs
+++ SuperHeroAPI/SuperHeroAPI/Program.cs
@@ -6,6 +6,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddPolicy(name: "SuperHeroOrigins",
+    policy =>
+    {
+        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+    }));
 
 var app = builder.Build();
 
