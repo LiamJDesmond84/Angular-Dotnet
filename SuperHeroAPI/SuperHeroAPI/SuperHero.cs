269d30afd070ed36f26f7f9ce7a42acec6edda6b
Liam James Desmond <liamjdesmond84@gmail.com>
2023-07-02 20:04:33 -0500

    Messing with initializing

--- SuperHeroAPI/SuperHeroAPI/SuperHero.cs
+++ SuperHeroAPI/SuperHeroAPI/SuperHero.cs
@@ -3,10 +3,10 @@
     public class SuperHero
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Place { get; set; }
+        public string Name { get; set; } = "";
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Place { get; set; } = "";
 
     }
 }
